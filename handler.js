fd0bed76a35d6d6409d104558ba60829ee06f9c1
Jesue Junior <jesuesousa@gmail.com>
2018-05-07 23:00:30 -0300

    Handler to consumer from kinesis

--- handler.js
+++ handler.js
@@ -1,17 +1,12 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-  console.log(event, context, callback);
+  console.log(JSON.stringify(event, null, 2));
+  event.Records.forEach(function(record) {
+    let payload = new Buffer(record.kinesis.data, 'base64') 
+    console.log('payload: ', payload);
+  });
+  context.succeed();
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  //TO-DO: Construct func to send to elastic
 };
